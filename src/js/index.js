a15efea0dddd7dda564c3f1a8b58db1e3450f97b
eccom18 <eccom18@gmail.com>
2022-10-17 19:39:51 +0300

    added layout, popup

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
 'use strict'
-
 import timer from './header'
 timer()
+
+const $POPUP = document.querySelector('#form_section')
+const $BODY = document.querySelector('body')
+const showPopUp = () => {
+  $POPUP.classList.toggle('open')
+  $BODY.classList.toggle('lock')
+}
+document
+  .querySelector('.fa-circle-xmark', '.cancel', '.fa-plus')
+  .addEventListener('click', showPopUp)
+document.querySelector('.cancel').addEventListener('click', showPopUp)
+document.querySelector('.fa-plus').addEventListener('click', showPopUp)
---
+++ src/js/popup.js
@@ -0,0 +1,5 @@
+export const showPopUp = () => {
+  console.log(1)
+}
+export let out1 = document.querySelector('.fa-circle-xmark')
+out1.addEventListener('click', showPopUp())
