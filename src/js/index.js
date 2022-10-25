4307497826f98f083a51c70cfb8f3792b1ac885d
eccom18 <eccom18@gmail.com>
2022-10-25 16:24:36 +0300

    added delet card

--- src/js/index.js
+++ src/js/index.js
@@ -11,31 +11,29 @@ const $AUTHOR = document.querySelector('.executor_input')
 const $COLUMNLIST = document.querySelector('.todo_column-list')
 const $CREATEBTN = document.querySelector('.create')
 const $NEWTOTAL = document.querySelector('.new_total')
+const $PROGRCOL = document.querySelector('todo_column-listProgr')
+let del
+let remove
+let cardArr = JSON.parse(localStorage.getItem('card')) || []
+let progressCard = JSON.parse(localStorage.getItem('card')) || []
 
-let cardArr = []
-
-for (let i = 0; i < $POPUPOPEN.length; i++) {
-  $POPUPOPEN[i].addEventListener('click', (e) => {
-    $POPUP.classList.toggle('open')
-    $BODY.classList.toggle('lock')
-    e.preventDefault()
-  })
+let createCard = (el) => {
+  let newCard = `
+    <div class="todo_card">
+      <div class="todo_card-title">${el.title}</div>
+      <div class="todo_card-description">${el.descr}</div>
+      <div class="todo_card-executor">
+        <span class="execuor_name">${el.author}</span>
+        <i class="fa-solid fa-arrow-right remove"></i>
+        <span class="card_delete"
+        ><i class="fa-sharp fa-solid fa-trash"></i
+      >
+      </span>
+      </div>
+    </div>
+    `
+  $COLUMNLIST.innerHTML += newCard
 }
-
-// верстка карточки
-
-$CREATEBTN.addEventListener('click', (e) => {
-  e.preventDefault()
-  firstArr()
-  render()
-  getRender()
-  // del()
-
-  $NEWTOTAL.textContent = cardArr.length
-  $TITLEINP.value = ''
-  $DESCRIPTIONINP.value = ''
-  $AUTHOR.value = ''
-})
 
 //создаём новый обьект и пушим его в массив
 let firstArr = () => {
@@ -53,84 +51,89 @@ let render = () => {
   localStorage.setItem('card', cardToJson)
 }
 
-//парсим массив
+//распарсим массив
 let getRender = () => {
-  let from = localStorage.getItem('card')
-  let dataFrom = JSON.parse(from)
-  if (dataFrom) {
-    sortArr(dataFrom)
+  if (cardArr) {
+    sortArr(cardArr)
   } else {
     $COLUMNLIST.innerHTML = ''
   }
 }
 
-let restart = () => {
-  $COLUMNLIST.innerHTML = ''
-  let fromRest = localStorage.getItem('card')
-  let restFrom = JSON.parse(fromRest)
-  let createCard1 = (el1) => {
-    let newCard1 = `
-      <div class="todo_card">
-        <div class="todo_card-title">${el1.title}</div>
-        <div class="todo_card-description">${el1.descr}</div>
-        <div class="todo_card-executor">
-          <span class="execuor_name">${el1.author}</span>
-          <span class="delete"
-          ><i class="fa-sharp fa-solid fa-trash card_delete"></i
-        ></span>
-        </div>
-      </div>
-      `
-    $COLUMNLIST.innerHTML += newCard1
-  }
-  let restSort = (restFrom) => {
-    restFrom.forEach((el1) => {
-      createCard1(el1)
-      cardArr.push(el1)
-      $NEWTOTAL.textContent = cardArr.length
-    })
-  }
-  if (restFrom) {
-    restSort(restFrom)
-  } else {
-    $COLUMNLIST.innerHTML = ''
-  }
+//добавление карточки
+for (let i = 0; i < $POPUPOPEN.length; i++) {
+  $POPUPOPEN[i].addEventListener('click', (e) => {
+    $POPUP.classList.toggle('open')
+    $BODY.classList.toggle('lock')
+  })
 }
-restart()
-
 //перебираем массив и делаем карточку
-let sortArr = (dataFrom) => {
+let sortArr = (cardArr) => {
   $COLUMNLIST.innerHTML = ''
-  dataFrom.forEach((el) => {
+  cardArr.forEach((el) => {
     createCard(el)
   })
 }
 
-let createCard = (el) => {
-  let newCard = `
-    <div class="todo_card">
-      <div class="todo_card-title">${el.title}</div>
-      <div class="todo_card-description">${el.descr}</div>
-      <div class="todo_card-executor">
-        <span class="execuor_name">${el.author}</span>
-        <span class="card_delete"
-        ><i class="fa-sharp fa-solid fa-trash"></i
-      ></span>
-      </div>
-    </div>
-    `
-  $COLUMNLIST.innerHTML += newCard
+// устанавливает дата-атрибуты
+
+function datasetCard() {
+  del = document.querySelectorAll('.card_delete')
+  for (let i = 0; i < del.length; i++) {
+    del[i].addEventListener('click', (e) => {
+      cardArr.splice(i, 1)
+      render()
+      restart()
+    })
+  }
+  // remove = document.querySelectorAll('.remove')
+  // for (let k = 0; k < remove.length; k++) {
+  //   remove[k].addEventListener('click', (event) => {
+  //     progressCard.push(cardArr.slice(k, 1))
+  //     progressCard.forEach((el) => {
+  //       let gth = `
+  //     <div class="todo_card">
+  //       <div class="todo_card-title">${el.title}</div>
+  //       <div class="todo_card-description">${el.descr}</div>
+  //       <div class="todo_card-executor">
+  //         <span class="execuor_name">${el.author}</span>
+  //         <i class="fa-solid fa-arrow-right remove"></i>
+  //         <span class="card_delete"
+  //         ><i class="fa-sharp fa-solid fa-trash"></i
+  //       >
+  //       </span>
+  //       </div>
+  //     </div>
+  //     `
+  //       // $PROGRCOL.innerHTML += gth
+  //     })
+  //   })
+  //   console.log(el)
+  // }
 }
 
-// выводим тотал
+// верстка карточки по клику
+$CREATEBTN.addEventListener('click', (e) => {
+  e.preventDefault()
+  firstArr()
+  render()
+  getRender()
+  datasetCard()
+  $NEWTOTAL.textContent = cardArr.length
+  $TITLEINP.value = ''
+  $DESCRIPTIONINP.value = ''
+  $AUTHOR.value = ''
+})
 
-// let del = (el) => {
-//   const $DEL = document.querySelectorAll('.card_delete')
-//   for (let i = 0; i < $DEL.length; i++) {
-//     console.log($DEL[i])
-//     $DEL[i].addEventListener('click', (e) => {
-//       localStorage.clear()
-//       console.log(e.target)
-//     })
-//   }
-// }
+//вывод карточек после рестарта
+
+let restart = () => {
+  if (cardArr) {
+    sortArr(cardArr)
+  } else {
+    $COLUMNLIST.innerHTML = ''
+  }
+  $NEWTOTAL.textContent = cardArr.length
+  datasetCard()
+}
+restart()
